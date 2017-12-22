573a5ec35156763ae1adf6739622a3692c0a7eab
van.duong <nhat-tuong-van.duong@etu.u-pec.fr>
2017-12-22 11:30:10 +0100

    Add static folder

--- repaspartage/urls.py
+++ repaspartage/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
