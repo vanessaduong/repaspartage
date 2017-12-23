a0a3983128b02f6a8a169d3b5c567ab1f0d3492f
van.duong <nhat-tuong-van.duong@etu.u-pec.fr>
2017-12-23 12:36:49 +0100

    Add Association page

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html', {})
+
+def association(request):
+    return render(request, 'blog/association.html', {})
